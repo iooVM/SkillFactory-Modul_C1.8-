01ffd3b0f4eceb5553d0ee0ee1acf51197c55530
Ширшаков Михаил Анатольевич <m.shirshakov@VP.local>
2021-07-08 15:27:33 +0300

    'SkillFactory:Modul_C1.8:nocomments'

--- Cat.py
+++ Cat.py
@@ -1,14 +1,35 @@
 class Cat:
-    def __init__(self,name,sex,age):
-    self.name = name
-    self.sex = sex
-    self.age = age
+    def __init__(self, name='x3',sex = 'Devochka',age = '0'):
+        self.name = name
+        self.sex = sex
+        self.age = age
 
     def get_name(self):
         return self.name
 
+    def set_name(self, name):
+        self.name = name
+
     def get_sex(self):
         return self.sex
 
+    def set_sex(self, sex):
+        self.sex = sex
+
     def get_age(self):
         return self.age
+
+    def set_age(self, age):
+        self.age = age
+
+
+if __name__ == '__main__':
+    cat1 = Cat()
+    cat1.set_name('tom')
+    cat1.set_sex('malchik')
+    cat1.set_age(15)
+    print(cat1.get_name())
+    print(cat1.get_sex())
+    print(cat1.get_age())
+
+
